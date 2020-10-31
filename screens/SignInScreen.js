0f4aa93cfae42837046a19cfdba0cd06a7974389
champchips <champza699@hotmail.com>
2020-11-01 00:34:51 +0700

    second

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -8,40 +8,55 @@ import {
   StatusBar,
   Image,
 } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
 const SignInScreen = (props) => {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../images/iBook.png")} />
       </View>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.welcome}>Welcome</Text>
-      <TextInput
-        placeholder="Username"
-        placeholderTextColor="rgba(19, 15, 64,0.5)"
-        returnKeyType="next"
-        autoCapitalize="none"
-        // onSubmitEditing={() => this.passwordInput.focus()}
-        autoCorrect={false}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        placeholderTextColor="rgba(19, 15, 64,0.5)"
-        returnKeyType="go"
-        secureTextEntry
-        style={styles.input}
-        // ref={(input) => this.passwordInput = input}
-      />
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => {
-          props.navigation.navigate("Login");
+      <View
+        style={{
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          backgroundColor: "white",
+          marginTop: 30,
+          alignItems: "center",
+          flexGrow: 1,
+          justifyContent: "center",
         }}
       >
-        <Text style={styles.buttonText}>Sign In</Text>
-      </TouchableOpacity>
+       <Ionicons name="ios-arrow-back" size={40} color="black" style={styles.iconBack}
+       onPress={() => {props.navigation.popToTop()}}
+       />
+        <Text style={styles.welcome}>Welcome</Text>
+        <TextInput
+          placeholder="Username"
+          placeholderTextColor="rgba(19, 15, 64,0.5)"
+          returnKeyType="next"
+          autoCapitalize="none"
+          // onSubmitEditing={() => this.passwordInput.focus()}
+          autoCorrect={false}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="rgba(19, 15, 64,0.5)"
+          returnKeyType="go"
+          secureTextEntry
+          style={styles.input}
+          // ref={(input) => this.passwordInput = input}
+        />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => {
+            props.navigation.navigate("Login");
+          }}
+        >
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -61,8 +76,9 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 50,
+    fontWeight: "bold",
     backgroundColor: "rgba(255,255,255,0.2)",
-    marginBottom: 20,
+    marginBottom: 30,
     paddingHorizontal: 20,
     borderColor: "#191927",
     borderWidth: 1,
@@ -70,25 +86,34 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 15,
     borderBottomLeftRadius: 15,
     borderBottomRightRadius: 15,
+    width: 300,
+    fontSize: 16,
   },
   welcome: {
     fontSize: 40,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 30,
     color: "black",
+    paddingRight: 100,
   },
   buttonContainer: {
-    backgroundColor: "#191927",
+    backgroundColor: "black",
     paddingVertical: 15,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    borderBottomLeftRadius: 15,
-    borderBottomRightRadius: 15,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    width: 300,
   },
   buttonText: {
     textAlign: "center",
     color: "#FFFFFF",
     fontWeight: "bold",
+    fontSize: 20,
+  },
+  iconBack: {
+    paddingRight: 275,
+    marginBottom: 30,
   },
 });
 export default SignInScreen;
